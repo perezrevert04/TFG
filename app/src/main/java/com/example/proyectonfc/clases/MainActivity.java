5608c4cd25e43189fd150afe6e82a084c38a9038
perezrevert04 <carlesperezmxt37@gmail.com>
2020-06-20 13:55:41 +0200

    Cambiada interfaz de la pantalla principal

--- app/src/main/java/com/example/proyectonfc/clases/MainActivity.java
+++ app/src/main/java/com/example/proyectonfc/clases/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.proyectonfc.clases;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.proyectonfc.R;
@@ -14,24 +17,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         dataBase = new DataBase(getApplicationContext(), "DB6.db", null, 1);
 
-        ImageButton buttonConfiguracion = (ImageButton) findViewById(R.id.buttonConfiguracion);
-        buttonConfiguracion.setOnClickListener(v -> {
-            Intent intent = new Intent(v.getContext(), Configuracion.class);
-            startActivityForResult(intent, 0);
-        });
-
-        ImageButton buttonUso = (ImageButton) findViewById(R.id.buttonUso);
-        buttonUso.setOnClickListener(v -> {
+        Button buttonStart = findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(v -> {
             Intent intent = new Intent(v.getContext(), IdentificacionProfesor.class);
             startActivityForResult(intent, 0);
         });
 
-        ImageButton buttonDatos = (ImageButton) findViewById(R.id.buttonDatos);
-        buttonDatos.setOnClickListener(v -> {
+        Button buttonManageSubjects = findViewById(R.id.buttonManageSubjects);
+        buttonManageSubjects.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), Configuracion.class);
+            startActivityForResult(intent, 0);
+        });
+
+
+        Button buttonConsultParts = findViewById(R.id.buttonConsultParts);
+        buttonConsultParts.setOnClickListener(v -> {
             Intent intent = new Intent(v.getContext(), DatosXml.class);
             startActivityForResult(intent, 0);
         });
