97aae4f2aac761a40b0162218a62852250f7e762
perezrevert04 <carlesperezmxt37@gmail.com>
2020-06-24 02:45:13 +0200

    Mejoras interfaz

--- app/src/main/java/com/example/proyectonfc/clases/AsignaturasProfesor.java
+++ app/src/main/java/com/example/proyectonfc/clases/AsignaturasProfesor.java
@@ -1,33 +1,26 @@
 package com.example.proyectonfc.clases;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.proyectonfc.R;
 import com.example.proyectonfc.db.DataBase;
-import com.example.proyectonfc.util.Asignatura;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class AsignaturasProfesor extends AppCompatActivity {
 
     DataBase dataBase;
-    private TextView asignaturaSeleccionada;
-    private  String asignatura;
     ArrayList<String> listaAsignaturas;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,48 +31,69 @@ public class AsignaturasProfesor extends AppCompatActivity {
         ListView asignaturas = (ListView) findViewById(R.id.listaAsignaturas);
 
         consultarListaAsignaturas();
-        ArrayAdapter adaptador=new ArrayAdapter
-                (this,android.R.layout.simple_list_item_1, (List) listaAsignaturas);
+        ArrayAdapter adaptador=new ArrayAdapter(this,android.R.layout.simple_list_item_1, (List) listaAsignaturas);
         asignaturas.setAdapter(adaptador);
 
 
-        Button buttonRecuperacion = (Button) findViewById(R.id.buttonRecuperacion);
-        buttonRecuperacion.setOnClickListener(v -> {
-            Intent intent = new Intent(v.getContext(), DatosAsignaturaRecuperacion.class);
-            intent.putExtra("ASIGNATURA", asignatura );
-            if (asignatura == null) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(AsignaturasProfesor.this);
-                builder.setTitle("No ha seleccionado ninguna Asignatura");
-                builder.setMessage("¡Debe de seleccionar alguna Asignatura!");
-                builder.setNeutralButton("¡Entendido!", null);
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }else {
-                startActivityForResult(intent, 0);
-            }
-        });
+//        buttonRecuperacion.setOnClickListener(v -> {
+//            Intent intent = new Intent(v.getContext(), DatosAsignaturaRecuperacion.class);
+//            intent.putExtra("ASIGNATURA", asignatura );
+//            if (asignatura == null) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(AsignaturasProfesor.this);
+//                builder.setTitle("No ha seleccionado ninguna Asignatura");
+//                builder.setMessage("¡Debe de seleccionar alguna Asignatura!");
+//                builder.setNeutralButton("¡Entendido!", null);
+//                AlertDialog dialog = builder.create();
+//                dialog.show();
+//            }else {
+//                startActivityForResult(intent, 0);
+//            }
+//        });
+//
+//        buttonNormal.setOnClickListener(v -> {
+//            Intent intent = new Intent(v.getContext(), DatosAsignaturaNormal.class);
+//            intent.putExtra("ASIGNATURA", asignatura );
+//            if (asignatura == null) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(AsignaturasProfesor.this);
+//                builder.setTitle("No ha seleccionado ninguna Asignatura");
+//                builder.setMessage("¡Debe de seleccionar alguna Asignatura!");
+//                builder.setNeutralButton("¡Entendido!", null);
+//                AlertDialog dialog = builder.create();
+//                dialog.show();
+//            }else {
+//                startActivityForResult(intent, 0);
+//            }
+//        });
 
-        Button buttonNormal = (Button) findViewById(R.id.buttonNormal);
-        buttonNormal.setOnClickListener(v -> {
-            Intent intent = new Intent(v.getContext(), DatosAsignaturaNormal.class);
-            intent.putExtra("ASIGNATURA", asignatura );
-            if (asignatura == null) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(AsignaturasProfesor.this);
-                builder.setTitle("No ha seleccionado ninguna Asignatura");
-                builder.setMessage("¡Debe de seleccionar alguna Asignatura!");
-                builder.setNeutralButton("¡Entendido!", null);
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }else {
-                startActivityForResult(intent, 0);
-            }
-        });
+        asignaturas.setOnItemClickListener( (adapterView, view, position, id) -> {
+            String asignatura = listaAsignaturas.get(position);
 
-        asignaturas.setOnItemClickListener((AdapterView.OnItemClickListener) (adapterView, view, position, id) -> {
-            asignatura = listaAsignaturas.get(position);
-            asignaturaSeleccionada = (TextView) findViewById(R.id.asignaturaSeleccionada);
-            asignaturaSeleccionada.setText(asignatura);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(asignatura).setMessage("Seleccione el tipo de asistencia:");
+            builder.setNegativeButton("Recuperación", (dialog, id1) -> {
+                asistenciaRecuperacion(asignatura, view.getContext());
+            });
+            builder.setPositiveButton("Normal", (dialog, id1) -> {
+                asistenciaNormal(asignatura, view.getContext());
+
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
         });
+    }
+
+    private void asistenciaNormal(String asignatura, Context context) {
+            Intent intent = new Intent(context, DatosAsignaturaNormal.class);
+            intent.putExtra("ASIGNATURA", asignatura );
+            startActivity(intent);
+    }
+
+    private void asistenciaRecuperacion(String asignatura, Context context) {
+        Intent intent = new Intent(context, DatosAsignaturaRecuperacion.class);
+        intent.putExtra("ASIGNATURA", asignatura );
+        startActivity(intent);
     }
 
     private void consultarListaAsignaturas() {
--- app/src/main/java/com/example/proyectonfc/clases/Configuracion.java
+++ app/src/main/java/com/example/proyectonfc/clases/Configuracion.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
